d0e53a02e8a6da5c036d376616aee6a78946fd57
takamoriWebtube <taka moribatako.com.0831@gmail.com>
2020-03-15 16:10:59 +0900

    first commit

--- back/src/katakata_api/app/Http/Controllers/CreditController.php
+++ back/src/katakata_api/app/Http/Controllers/CreditController.php
@@ -3,40 +3,61 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Stripe;
 
 class CreditController extends Controller
 {
     public function pay(Request $request)
     {
-        // Set your secret key: remember to change this to your live secret key in production
-        // See your keys here: https://dashboard.stripe.com/account/apikeys
+
         \Stripe\Stripe::setApiKey(config('stripe_key'));
 
         try {
             // Token is created using Checkout or Elements!
             // Get the payment token ID submitted by the form:
             $token = $request->input('stripeToken');
-            $charge = \Stripe\Charge::create([
+            $charge = Stripe\Charge::create([
                 'amount' => $request->input('amount'),
                 'currency' => 'jpy',
                 'description' => 'Example charge',
                 'source' => $token,
             ]);
-        }catch (\Stripe\Error\Card $e){ //元は\Stripe\Error\Card
+        }catch (Stripe\Exception\CardException $e){
             // 決済に失敗したらエラーメッセージを返す
             return response()->json([
                 'success' => false,
                 'errors' => $e->getMessage()
             ], 422);
         }
+        // Set your secret key: remember to change this to your live secret key in production
+        // See your keys here: https://dashboard.stripe.com/account/apikeys
+        // \Stripe\Stripe::setApiKey(config('stripe_key'));
 
-        $user_wallet = $this->exchange->cashExchange($request);
+        // try {
+        //     // Token is created using Checkout or Elements!
+        //     // Get the payment token ID submitted by the form:
+        //     $token = $request->input('stripeToken');
+        //     $charge = \Stripe\Charge::create([
+        //         'amount' => $request->input('amount'),
+        //         'currency' => 'jpy',
+        //         'description' => 'Example charge',
+        //         'source' => $token,
+        //     ]);
+        // }catch (\Stripe\Error\Card $e){ //元は\Stripe\Error\Card
+        //     // 決済に失敗したらエラーメッセージを返す
+        //     return response()->json([
+        //         'success' => false,
+        //         'errors' => $e->getMessage()
+        //     ], 422);
+        // }
 
-        return response()->json([
-            'success' => true,
-            'data' => $user_wallet,
-            'message' => '現金からポイントに換金が完了しました'
-        ], 200);
+        // $user_wallet = $this->exchange->cashExchange($request);
+
+        // return response()->json([
+        //     'success' => true,
+        //     'data' => $user_wallet,
+        //     'message' => '現金からポイントに換金が完了しました'
+        // ], 200);
 
     }
 }
